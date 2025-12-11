9a2286a8781eb4c23b808df14e8546dacfcd8d63
Wexcea <wexcea@gmail.com>
2025-12-11 10:22:35 +0700

    Update data.js

--- test/data.js
+++ test/data.js
@@ -4,434 +4,477 @@ const scripts = {
     name: "BlockSpin",
     options: [
       {
-        title: "Farm",
+        title: "Hermanos Hub — Farm",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/hermanos-dev/hermanos-hub/refs/heads/main/BlockSpin/blockspin-farm.lua'))()`
       },
       {
-        title: "PvP",
+        title: "Hermanos Hub — PvP",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/hermanos-dev/hermanos-hub/refs/heads/main/BlockSpin/blockspin-pvp.lua'))()`
       }
     ]
   },
+
   "attack_on_titan_revolution": {
     name: "Attack on Titan Revolution",
     options: [
       {
-        title: "Default",
+        title: "Bomba — Default",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/LunarZR/bomba/refs/heads/main/AOTR1'))()`
       }
     ]
   },
+
   "world_zero": {
     name: "World Zero",
     options: [
       {
-        title: "Default",
+        title: "World Zero — Default",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/justdoingrandomstuff/World-Zero/refs/heads/main/autofarm'))()`
       }
     ]
   },
+
   "jailbreak": {
     name: "JailBreak",
     options: [
       {
-        title: "Default",
+        title: "ProjectAuto — Default",
         code: `loadstring(game:HttpGet('http://scripts.projectauto.xyz/AutoRobV6'))()`
       }
     ]
   },
+
   "blade_ball": {
     name: "Blade ball",
     options: [
       {
-        title: "ArgonHubX",
+        title: "Argon Hub X — Loader",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/AgentX771/ArgonHubX/main/Loader.lua'))()`
       },
       {
-        title: "NatHub",
+        title: "NatHub — Main Script",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/NatHub.lua'))();`
       }
     ]
   },
+
   "arise_crossover": {
     name: "Arise Crossover",
     options: [
       {
-        title: "SpeedHubX",
+        title: "Speed Hub X — Main Script",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua', true))();`
       },
       {
-        title: "AriseCrossover",
+        title: "Goomba Hub — Arise Crossover",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/JustLevel/goombahub/main/AriseCrossover.lua'))()`
       },
       {
-        title: "VenuzHub",
+        title: "Venuz Hub — Loader",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/x2Kotaro/Venuz-hub/main/Loader.lua'))()`
       }
     ]
   },
+
   "be_fish": {
     name: "Be Fish",
     options: [
       {
-        title: "Swee_Hub",
+        title: "Swee Hub — Loader",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/AkinaRulezx/Swee_Hub/main/LoadSWE', true))()`
       },
       {
-        title: "Main",
+        title: "BeaFish — Main Script",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/AkinaRulezx/beafish/main/main%20script', true))()`
       }
     ]
   },
+
   "driving_empire": {
     name: "Driving Empire",
     options: [
       {
-        title: "NewBrand",
+        title: "New Brand — AutoFarmer",
         code: `loadstring(game:HttpGet('https://rawscripts.net/raw/NEW-BRAND-Driving-Empire-Car-Racing-AutoFarmer-and-AutoRacer-23697'))()`
       },
       {
-        title: "Deposible",
+        title: "Deposible Hub — Main Script",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/deposible/deposible.github.io/refs/heads/main/Driving%20Empire.lua'))()`
       }
     ]
   },
+
   "die_of_death": {
     name: "Die of Death",
     options: [
       {
-        title: "Default",
+        title: "Red Hub — Default",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/thuker-evader/Die-of-death/refs/heads/main/Red%20hub'))()`
       }
     ]
   },
+
   "king_legacy": {
     name: "King Legacy",
     options: [
       {
-        title: "Default",
+        title: "Ls Zee Hub — Default",
         code: `loadstring(game:HttpGet('https://zuwz.me/Ls-Zee-Hub-KL'))()`
       }
     ]
   },
+
   "new_race_car_dealership_tycoon": {
     name: "NEW RACE! Car Dealership Tycoon",
     options: [
       {
-        title: "Default",
+        title: "Default Script — Loader",
         code: `loadstring(game:HttpGet('https://norepinefrina.com'))()`
       }
     ]
   },
+
   "restaurant_tycoon_3": {
     name: "Restaurant Tycoon 3",
     options: [
       {
-        title: "Chiyo",
+        title: "Chiyo Hub — Loader",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/kaisenlmao/loader/refs/heads/main/chiyo.lua'))();`
       }
     ]
   },
+
   "volleyball_legends": {
     name: "Volleyball Legends",
     options: [
       {
-        title: "Default",
+        title: "SterlingNew — Default",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/Zayn31214/name/refs/heads/main/SterlingNew'))()`
       }
     ]
   },
+
   "racket_rivals": {
     name: "Racket Rivals",
     options: [
       {
-        title: "Default",
+        title: "ScriptsRBX — Main Script",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/ScriptsRBXdotCom/scripts/refs/heads/main/racketrivals'))()`
       }
     ]
   },
+
   "race_clicker": {
     name: "Race Clicker",
     options: [
       {
-        title: "Default",
+        title: "Gumanba — Default",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/gumanba/Scripts/main/RaceClicker'))()`
       }
     ]
   },
+
   "one_piece_mythical": {
     name: "One Piece: Mythical",
     options: [
       {
-        title: "AFKChest",
+        title: "AFK Chest — Auto Chest",
         code: `_G.hopServer=true; loadstring(game:HttpGet('https://raw.githubusercontent.com/gunchaznon-ops/AutoChest/refs/heads/main/Auto.lua'))()`
       },
       {
-        title: "FullFunctions",
+        title: "Full Functions Hub — Loader",
         code: `loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/8b80da8a731e75bc386e7f01c1151a57.lua'))()`
       }
     ]
   },
+
   "meme_sea": {
     name: "Meme Sea",
     options: [
       {
-        title: "Default",
+        title: "TLREDZ — Main Script",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/tlredz/Scripts/refs/heads/main/main.luau'))()`
       }
     ]
   },
+
   "garden_tower_defense": {
     name: "Garden Tower Defense",
     options: [
       {
-        title: "Default",
+        title: "Pastefy — Loader",
         code: `loadstring(game:HttpGet('https://pastefy.app/ULaWpxKm/raw'))()`
       }
     ]
   },
+
   "your_goblin_mine": {
     name: "Your Goblin Mine",
     options: [
       {
-        title: "Default",
+        title: "Pastebin — Default",
         code: `PlaceIds=[127590941940388,136409236554789]; loadstring(game:HttpGet('https://pastebin.com/raw/YX9LTtab'))()`
       }
     ]
   },
+
   "unnamed_shooter": {
     name: "Unnamed Shooter",
     options: [
       {
-        title: "Default",
+        title: "KaliHub — Loader",
         code: `loadstring(game:HttpGet('https://kalihub.xyz/loader.lua'))()`
       }
     ]
   },
+
   "anime_crusaders": {
     name: "Anime Crusaders",
     options: [
       {
-        title: "Default",
+        title: "Pastefy — Default",
         code: `loadstring(game:HttpGet('https://pastefy.app/ULaWpxKm/raw'))()`
       }
     ]
   },
+
   "anime_evolution": {
     name: "Anime Evolution",
     options: [
       {
-        title: "IQ",
+        title: "IQ Hub — Main Script",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/checkurasshole/Script/refs/heads/main/IQ'))()`
       },
       {
-        title: "README",
+        title: "AE Hub — Readme Loader",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/rystery/aE/refs/heads/main/README.md'))()`
       }
     ]
   },
+
   "anime_eternal": {
     name: "ANIME ETERNAL",
     options: [
       {
-        title: "A",
+        title: "Gehlee Hub — Script A",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/OhhMyGehlee/sh/refs/heads/main/a'))()`
       },
       {
-        title: "ER",
+        title: "Gehlee Hub — Script ER",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/OhhMyGehlee/et/refs/heads/main/er'))()`
       }
     ]
   },
+
   "ninja_legends": {
     name: "Ninja Legends",
     options: [
       {
-        title: "Default",
+        title: "Proxima Hub — Main Script",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/Main.lua', true))()`
       }
     ]
   },
+
   "muscle_legends": {
     name: "Muscle Legends",
     options: [
       {
-        title: "Default",
+        title: "Tokattk — Default",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/2581235867/21/refs/heads/main/By%20Tokattk'))()`
       }
     ]
   },
+
   "be_a_beggar": {
     name: "Be a Beggar!",
     options: [
       {
-        title: "Default",
+        title: "Hyl9 — Script",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/danangori/Be-a-Beggar/refs/heads/main/2025-Hyl9'))()`
       }
     ]
   },
+
   "a_universal_time": {
     name: "A Universal Time",
     options: [
       {
-        title: "Default",
+        title: "Akatsuki Hub — Loader",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/AkatsukiHub1/A-Universal-Time/refs/heads/main/README.md'))()`
       }
     ]
   },
+
   "[upd]_bike_of_hell": {
     name: "[UPD] Bike of Hell",
     options: [
       {
-        title: "Default",
+        title: "HSKDEV — Default",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/HSKDEVfr/BikeofHell/refs/heads/main/bikeofhell'))()`
       }
     ]
   },
+
   "a_dusty_trip_car_quest": {
     name: "a dusty trip [CAR QUEST]",
     options: [
       {
-        title: "Default",
+        title: "DustyTrip — Beta Loader",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/AbdouGG/dustytripv1/main/beta'))()`
       }
     ]
   },
+
   "eat_the_world": {
     name: "Eat the World",
     options: [
       {
-        title: "Default",
+        title: "Luminary Hub — Loader",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Scripts/Main%20Loader.lua'))()`
       }
     ]
   },
+
   "[gtr]_sakon_town_boxing": {
     name: "[GTR] Sakon Town Boxing",
     options: [
       {
-        title: "Default",
+        title: "Winnable Hub — Loader",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/Winnable-Hub/Sakon/refs/heads/main/Loading.Lua'))()`
       }
     ]
   },
+
   "counter_blox": {
     name: "Counter Blox",
     options: [
       {
-        title: "Default",
+        title: "EgorWare — Aurora",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/egorware/scripts/refs/heads/main/aurora_but_eng.lua'))()`
       }
     ]
   },
+
   "[don]_blue_lock_rivals": {
     name: "[DON] Blue Lock: Rivals",
     options: [
       {
-        title: "Script1",
+        title: "Reo Script — No CD",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/BexruzScripts/Blue-lock-no-cd-reo-get-any-style-script.-WORKS-WITH-WORLD-CLASSES-/refs/heads/main/script.lua'))()`
       },
       {
-        title: "Script2",
+        title: "Obfuscate Kid — Script 2",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/gonnered/obfuscate-kid/refs/heads/main/obfuscated_script-1758340500011.lua.txt'))()`
       }
     ]
   },
+
   "murder_mystery_2": {
     name: "Murder Mystery 2",
     options: [
       {
-        title: "Default",
+        title: "Vertex Hub — Loader",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/vertex-peak/vertex/refs/heads/main/loadstring'))()`
       }
     ]
   },
+
   "7_days_to_live": {
     name: "7 DAYS TO LIVE",
     options: [
       {
-        title: "Default",
+        title: "Luarmor — Loader",
         code: `loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/255ac567ced3dcb9e69aa7e44c423f19.lua'))()`
       }
     ]
   },
+
   "[odm_gear]_snk_rumbling": {
     name: "[ODM GEAR] SNK: Rumbling",
     options: [
       {
-        title: "Default",
+        title: "Agreed69 — Universal Aimbot",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/agreed69-scripts/open-src-scripts/refs/heads/main/Universal%20Aimbot.lua',true))()`
       }
     ]
   },
+
   "arsenal": {
     name: "Arsenal",
     options: [
       {
-        title: "Default",
+        title: "Unbound Hub — Loader",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/samerop/unbound-hub/main/unbound-hub.lua'))()`
       }
     ]
   },
+
   "cut_trees_world_3": {
     name: "Cut Trees [WORLD 3]",
     options: [
       {
-        title: "Default",
+        title: "Konglomerate — Loader",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/Konglomerate/Script/Main/Loader'))()`
       }
     ]
   },
+
   "combat_warriors": {
     name: "Combat Warriors",
     options: [
       {
-        title: "Default",
+        title: "Luarmor — Loader",
         code: `loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/e115b107e044a8cfc35b87ce573d558f.lua'))()`
       }
     ]
   },
+
   "[2x]_dragon_soul_anime_mmo": {
     name: "[2x] Dragon Soul | Anime MMO",
     options: [
       {
-        title: "Default",
+        title: "Imp Hub — Loader",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/alan11ago/Hub/refs/heads/main/ImpHub.lua'))()`
       }
     ]
   },
+
   "build_ur_base": {
     name: "Build Ur Base",
     options: [
       {
-        title: "Default",
+        title: "Pastebin — Loader",
         code: `loadstring(game:HttpGet('https://pastebin.com/raw/BBK7PLsJ', true))()`
       }
     ]
   },
+
   "build_a_cart": {
     name: "Build A Cart",
     options: [
       {
-        title: "Default",
+        title: "Gumanba — Loader",
         code: `loadstring(game:HttpGet('https://raw.githubusercontent.com/gumanba/Scripts/m'))()`
       }
     ]
   },
+
   "car_battles": {
     name: "CAR BATTLES",
     options: [
       {
-        title: "Default",
+        title: "User75335836 — Script",
         code: `loadstring(game:HttpGet('https://gist.githubusercontent.com/user75335836/23d735b04864c6505e2eb68513f1d947/raw/116600375b3a77b7412c68a3e26db59b653e4bf1/gistfile1.txt'))()`
       }
     ]
   },
+
   "sharkbite_2": {
     name: "SharkBite 2",
     options: [
       {
-        title: "Default",
+        title: "Pastebin — Default",
         code: `loadstring(game:HttpGet('https://pastebin.com/raw/926hdcsB'))()`
       }
     ]
